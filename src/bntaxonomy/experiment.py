9c969ccf00a3cbf0ece65f7403b11e312f2102f5
Kyungduk Moon <kaleb.moon@postech.ac.kr>
2025-10-06 22:18:56 +0900

    Removed unused codes in experiment.py after rebuilding iface

--- src/bntaxonomy/experiment.py
+++ src/bntaxonomy/experiment.py
@@ -6,13 +6,14 @@ from colomoto.minibn import BooleanNetwork
 
 from bntaxonomy.utils.control import CtrlResult
 from bntaxonomy.utils.log import main_logger
-import bntaxonomy.utils.log as log_utils
 from bntaxonomy.utils.control import suppress_console_output
 
 from bntaxonomy.iface import registered_tools
 
+
 class ExperimentHandler:
     __next_id = 1
+
     def __init__(
         self,
         name: str,
@@ -55,6 +56,7 @@ class ExperimentHandler:
         # Load the Boolean network for experiments
         if use_propagated:
             from bntaxonomy.iface.mpbn import propagate_bn
+
             self.bn = propagate_bn(self.org_bnet, self.inputs)
             self.inputs = {}
         else:
@@ -75,7 +77,6 @@ class ExperimentHandler:
         self.sm_attrs = None
         self.primes = None
         self.cabean = None
-
 
     def postprocess(self, ctrl_result: CtrlResult):
         ctrl_result.sort_d_list()
@@ -99,31 +100,6 @@ class ExperimentHandler:
             os.remove("program_instance.asp")
         return ctrl_result
 
-
-    ### optboolnet
-
-    def ctrl_optboolnet_sync_attr(self, **kwargs):
-        from bntaxonomy.iface.optboolnet import (
-            make_CNFBooleanNetwork_iface,
-            ctrl_optboolnet_sync_attr_iface,
-        )
-
-        cnf_bn = make_CNFBooleanNetwork_iface(self.bn, self.inputs, self.target)
-        results = ctrl_optboolnet_sync_attr_iface(self.name, cnf_bn, self.max_size)
-
-        return self.postprocess(results)
-
-    def ctrl_optboolnet_fp(self, **kwargs):
-        from bntaxonomy.iface.optboolnet import (
-            make_CNFBooleanNetwork_iface,
-            ctrl_optboolnet_fp_iface,
-        )
-
-        cnf_bn = make_CNFBooleanNetwork_iface(self.bn, self.inputs, self.target)
-        results = ctrl_optboolnet_fp_iface(self.name, cnf_bn, self.max_size)
-
-        return self.postprocess(results)
-
     def run_tools(self, filter_tools):
         for toolcls in registered_tools():
             if filter_tools and toolcls.name not in filter_tools:
@@ -137,10 +113,13 @@ class ExperimentHandler:
             elif toolcls.bn_type == "colomoto.BooleanNetwork":
                 bninp = self.bn
             else:
-                raise TypeError(f"{toolcls.name}: Unknown BN type input {toolcls.bn_type}")
+                raise TypeError(
+                    f"{toolcls.name}: Unknown BN type input {toolcls.bn_type}"
+                )
             with suppress_console_output():
-                res = toolcls.run(bninp, self.max_size, self.target,
-                                    self.exclude, *args)
+                res = toolcls.run(
+                    bninp, self.max_size, self.target, self.exclude, *args
+                )
             res = CtrlResult(toolcls.name, res)
             self.postprocess(res)
 
@@ -148,109 +127,3 @@ class ExperimentHandler:
             if toolcls.uses_cache:
                 main_logger.info(f"Cleaning cache for {toolcls.name}")
                 toolcls.free_experiment(self.expid)
-
-
-    ### PyBoolNet
-    def make_primes(self):
-        from bntaxonomy.iface.pbn import make_pbn_primes_iface, PRIME_JSON_FILE
-
-        if self.primes is None:
-            if self.load_precompute:
-                try:
-                    with open(f"{self.input_path}/{PRIME_JSON_FILE}") as _f:
-                        self.primes = json.load(_f)
-                    main_logger.info("Loaded precomputed pyboolnet primes successfully")
-                except:
-                    main_logger.info("Loading precomputed pyboolnet primes fails")
-                    self.primes = make_pbn_primes_iface(self.bnet_fname)
-            else:
-                self.primes = make_pbn_primes_iface(self.bnet_fname)
-            with open(f"{self.input_path}/{PRIME_JSON_FILE}", "w") as _f:
-                json.dump(self.primes, _f)
-
-
-    ### pystablemotifs
-
-    def make_sm_attrs(self):
-        from bntaxonomy.iface.stablemotif import make_sm_attrs_iface
-
-        self.make_primes()
-        if self.sm_attrs is None:
-            self.sm_attrs = make_sm_attrs_iface(self.primes)
-
-    def ctrl_pystablemotif_brute_force(self, **kwargs):
-        from bntaxonomy.iface.stablemotif import ctrl_sm_brute_force_iface
-
-        self.make_primes()
-
-        results = ctrl_sm_brute_force_iface(
-            self.primes, self.target, self.max_size, **kwargs
-        )
-        return self.postprocess(results)
-
-    def ctrl_pystablemotif_trapspace(
-        self, target_method: str, driver_method: str, **kwargs
-    ):
-        from bntaxonomy.iface.stablemotif import ctrl_sm_trapspace_iface
-
-        assert target_method in ["merge", "history"]
-        assert driver_method in ["minimal", "internal"]
-        self.make_sm_attrs()
-
-        results = ctrl_sm_trapspace_iface(
-            self.sm_attrs,
-            self.target,
-            target_method,
-            driver_method,
-            self.max_size,
-            **kwargs,
-        )
-        return self.postprocess(results)
-
-    ### CABEAN
-    def make_cabean(self):
-        from bntaxonomy.iface.cabean import (
-            make_cabean_iface,
-            CABEAN_OUT_MEMORY,
-            ATTR_JSON_FILE,
-        )
-
-        # if experienced out of memory, skip retrying
-        if self.cabean == CABEAN_OUT_MEMORY:
-            return
-        if self.cabean is None:
-            try:
-                self.cabean = make_cabean_iface(self.bn)
-                if self.load_precompute:
-                    try:
-                        with open(f"{self.input_path}/{ATTR_JSON_FILE}") as _f:
-                            self.cabean.load_precomputed_attr(json.load(_f))
-                        main_logger.info("Loaded precomputed cabean successfully")
-                    except:
-                        main_logger.info("Loading precomputed cabean fails")
-                        self.cabean.compute_attractors()
-                else:
-                    self.cabean.compute_attractors()
-                with open(f"{self.input_path}/{ATTR_JSON_FILE}", "w") as _f:
-                    json.dump(self.cabean.attractors, _f)
-            except Exception as e:
-                main_logger.info(f"Error loading cabean: {e}")
-                self.cabean = CABEAN_OUT_MEMORY
-                return
-
-    def ctrl_cabean_target_control(self, method: str, _debug=False, **kwargs):
-        assert method in ["ITC", "TTC", "PTC"]
-        self.make_cabean()
-
-        from bntaxonomy.iface.cabean import ctrl_target_control_iface, CABEAN_OUT_MEMORY
-
-        if self.cabean == CABEAN_OUT_MEMORY:
-            # if experienced out of memory, return empty result
-            results = CtrlResult(f"CABEAN[{method}]", [])
-        else:
-            results = ctrl_target_control_iface(
-                self.cabean, self.target, method, _debug, **kwargs
-            )
-        return self.postprocess(results)
-
-
