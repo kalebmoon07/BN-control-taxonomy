03e1b1f8ba250239dab383b38441488c0f298b45
Kyungduk Moon <kaleb.moon@postech.ac.kr>
2026-01-29 22:45:11 +0900

    ActoNet allowing empty fixpoint

--- src/bntaxonomy/iface/actonet.py
+++ src/bntaxonomy/iface/actonet.py
@@ -1,8 +1,15 @@
+import functools
 from bntaxonomy.iface import register_tool
 from bntaxonomy.utils.control import refine_pert
 from bntaxonomy.utils.log import time_check
 
 from actonet import ActoNet
+class myActoNet(ActoNet):
+    def __init__(self, bn, inputs=dict()):
+        super().__init__(bn, inputs)
+        # a fixpoint need not exist; if a control induces no fixpoint, it is a valid control
+        self.controls = functools.partial(self.controls, existential=False)
+
 
 @register_tool
 class ActoNetFP:
@@ -12,6 +19,6 @@ class ActoNetFP:
     @time_check
     @staticmethod
     def run(bn, max_size, target, exclude, inputs={}):
-        a = ActoNet(bn, inputs)
+        a = myActoNet(bn, inputs)
         r = a.reprogramming_fixpoints(target, maxsize=max_size, ignore=exclude)
         return refine_pert(r)
